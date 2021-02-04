ffdc4c82d7d2967a945fbc8cbbc81d5dbcbec820
Coder-XO <coderless@gmail.com>
2021-02-04 16:03:48 +0800

    修改了注释内容

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var url = require('url')
 var port = process.argv[2]
 
 if(!port){
-  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  console.log('请指定端口号\nnode server.js 8888 类似这样')
   process.exit(1)
 }
 
@@ -17,9 +17,9 @@ var server = http.createServer(function(request, response){
   var query = parsedUrl.query
   var method = request.method
 
-  /******** 从这里开始看，上面不要看 ************/
 
-  console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+
+  console.log('有人发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
   if(path === '/'){
     response.statusCode = 200
@@ -38,7 +38,7 @@ var server = http.createServer(function(request, response){
     response.end()
   }
 
-  /******** 代码结束，下面不要看 ************/
+
 })
 
 server.listen(port)
